c62950ce434f947773a6dbb86b9634089797a420
123krissh <krisshsuthar348@gmail.com>
2025-03-31 18:01:05 +0530

    add cloudinary,subscribe,admin Features

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -31,7 +31,7 @@ const orderSchema = new mongoose.Schema(
       address: { type: String, required: true },
       city: { type: String, required: true },
       pinCode: { type: String, required: true },
-      state: {type: String, required: true},
+      state: { type: String, required: true },
       country: { type: String, required: true },
     },
     paymentMethod: {
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const subscribeRoute = require("./routes/subscribeRoute");
+const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 app.use(express.json());
@@ -27,6 +31,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api", subscribeRoute);
+
+// Admin
+app.use("/api/admin/users", adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
