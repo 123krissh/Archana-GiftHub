df4448e794c914cab9110a38159a8a051d0b808e
123krissh <krisshsuthar348@gmail.com>
2025-04-09 00:20:28 +0530

    final

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -200,7 +200,7 @@ const NewArrivals = () => {
                     draggable="false"
                     />
 
-                    <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
+                    <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-black p-4 rounded-b-lg">
                         <Link to={`/product/${product._id}`} className="block">
                         <h4 className="font-medium">{product.name}</h4>
                         <p className="mt-1">Starting from ₹{product.discountPrice}</p>
